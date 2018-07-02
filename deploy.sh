7ec93e2679df26d1950459171504ac78540b2a83
mohamed.labouardy@gmail.com <mohamed@labouardy.com>
2018-07-02 15:33:42 +0200

    create cloudwatch event rule

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-AWS_REGION="us-east-1"
+## Override
 JENKINS_HOST=""
 JENKINS_USERNAME=""
 JENKINS_PASSWORD=""
 JENKINS_JOB=""
+CRON_EXPRESSION="cron(0 8 * * ? *)"
+## Global variables
+AWS_REGION="us-east-1"
+FUNCTION_NAME="RestartJob"
 
 echo "Building binary"
 GOOS=linux go build -o main main.go
@@ -12,21 +16,28 @@ GOOS=linux go build -o main main.go
 echo "Generating deployment package"
 zip deployment.zip main
 
-
 echo "Creating IAM Role"
-ARN=$(aws iam create-policy --policy-name RestartJobRole --policy-document file://policy.json | jq -r '.Policy.Arn')
-result=$(aws iam create-role --role-name RestartJobRole --assume-role-policy-document file://role.json | jq -r '.Role.Arn')
-aws iam attach-role-policy --role-name RestartJobRole --policy-arn $ARN
+POLICY_ARN=$(aws iam create-policy --policy-name $FUNCTION_NAME --policy-document file://policy.json | jq -r '.Policy.Arn')
+ROLE_ARN=$(aws iam create-role --role-name $FUNCTION_NAME --assume-role-policy-document file://role.json | jq -r '.Role.Arn')
+aws iam attach-role-policy --role-name $FUNCTION_NAME --policy-arn $POLICY_ARN
 
 echo "Creating Lambda function"
-aws lambda create-function --function-name RestartJob --runtime go1.x \
-    --handler main --role $ARN\
+FUNCTION_ARN=$(aws lambda create-function --function-name $FUNCTION_NAME --runtime go1.x \
+    --handler main --role $ROLE_ARN \
     --zip-file fileb://./deployment.zip \
-    --environment Variables="{JENKINS_HOST=$JENKINS_HOST, JENKINS_USERNAME=$JENKINS_USERNAME, \
-                              JENKINS_PASSWORD=$JENKINS_PASSWORD, JENKINS_JOB=$JENKINS_JOB}" \
-    --region $AWS_REGION
+    --environment Variables="{JENKINS_HOST=$JENKINS_HOST,JENKINS_USERNAME=$JENKINS_USERNAME,JENKINS_PASSWORD=$JENKINS_PASSWORD,JENKINS_JOB=$JENKINS_JOB}" \
+    --region $AWS_REGION | jq -r '.FunctionArn')
 
 echo "Creating CloudWatch Event rule"
+RULE_ARN=$(aws events put-rule --name launch-container-daily --schedule-expression ''"$CRON_EXPRESSION"'' | jq -r '.RuleArn')
+aws lambda add-permission --function-name $FUNCTION_NAME \
+    --statement-id 1 \
+    --action 'lambda:InvokeFunction' \
+    --principal events.amazonaws.com \
+    --source-arn $RULE_ARN
+sed -i '.bak' 's/FUNCTION_ARN/'"$FUNCTION_ARN"'/g' targets.json
+aws events put-targets --rule launch-container-daily --targets file://targets.json
 
-# invoke it with cli to test
-#show jenkins  
+
+echo "Cleaning up"
+rm main deployment.zip *.bak
